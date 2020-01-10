71171e1d33a25187a17726ef587c4b5c50602c05
CryptoMF <andrew.rall@gmail.com>
2020-01-10 02:56:57 +0200

    Create setperms.sh

---
+++ setperms.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This file will correctly set the file permissions for Frostybot on Ubuntu server (www-data). 
+# If your web server is using an account other than www-data, replace the account name below.
+
+BOTPATH=`dirname "$(readlink -f "$0")"`
+echo "$BOTPATH"
+
+sudo chgrp -R www-data "$BOTPATH"
+sudo chmod -R 640 "$BOTPATH"
+sudo chmod -R 660 "$BOTPATH/cache"
+sudo chmod -R 660 "$BOTPATH/db"
+sudo chmod -R 660 "$BOTPATH/log"
+sudo chmod 750 "$BOTPATH/frostybot"
+sudo chmod 750 "$BOTPATH/setperms.sh"
+sudo chmod -R +X "$BOTPATH"
